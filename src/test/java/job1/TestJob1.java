1d540734110692cb611ab5b9f381315350d5bfb9
Meyke <michele.tedesco487@virgilio.it>
2019-04-29 15:05:24 +0200

    aggiunto job 1

---
+++ src/test/java/job1/TestJob1.java
@@ -0,0 +1,79 @@
+package job1;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Test;
+
+public class TestJob1 {
+
+	@Test
+	public void testMapper() throws Exception {
+
+		MapDriver <LongWritable, Text, Text, Stock> mp = new MapDriver<LongWritable, Text, Text, Stock>();
+		mp = mp.withMapper(new TopNStocksMapper());
+		mp.withInput(new LongWritable(1), new Text("AHH,11.5,11.5799999237061,8.49315452575684,11.25,11.6800003051758,4633900,2013-05-08"));
+		System.out.println(mp.run().toString());
+
+	}
+
+	@Test
+	public void testReducer() throws Exception {
+
+		List<Stock> values = new ArrayList<Stock>();
+
+
+		/*
+		 * 
+	           AHH,11.5799999237061,11.25,11.6800003051758,4633900,2013-05-08
+     		   AHH,11.5500001907349,11.5,11.6599998474121,275800,2013-05-09
+			   AHH,11.6000003814697,11.5,11.6000003814697,277100,2013-05-10
+		 */
+		
+		Stock stock1 = new Stock(new Text("AHH"), new FloatWritable((float) 11.6000003814697), new FloatWritable((float) 11.5), 
+				new FloatWritable((float) 11.6000003814697), new IntWritable(277100), new Text("2013-05-10"));
+		
+		Stock stock2 = new Stock(new Text("AHH"), new FloatWritable((float) 11.5799999237061), new FloatWritable((float) 11.25), 
+				new FloatWritable((float) 11.6800003051758), new IntWritable(4633900), new Text("2013-05-08"));
+
+		Stock stock3 = new Stock(new Text("AHH"), new FloatWritable((float) 11.5500001907349), new FloatWritable((float) 11.5), 
+				new FloatWritable((float) 11.6599998474121), new IntWritable(275800), new Text("2013-05-09"));
+
+
+		values.add(stock1);
+		values.add(stock2);
+		values.add(stock3);
+
+
+		ReduceDriver <Text, Stock, Text, StockToOutput> rd = new ReduceDriver<Text, Stock, Text, StockToOutput>();
+		rd = rd.withReducer(new TopNStocksReducer()); //Sets the reducer object to use for this test
+		rd.withInput(new Text("AHH"), values); // imposta l'imput da inviare al reducer setInput(K1 key, List<V1> values)
+		System.out.println(rd.run().toString());
+
+	}
+
+	/*
+	    @Test
+	    public void testMapReduce() throws Exception {
+
+	        new MapReduceDriver<Object, Text, Text, IntWritable, Text, IntWritable>()
+	                .withMapper(new WordCount.TokenizerMapper())
+	                .withInput(NullWritable.get(), new Text("foo bar foo"))
+	                .withReducer(new WordCount.IntSumReducer())
+	                .withOutput(new Text("bar"), new IntWritable(1))
+	                .withOutput(new Text("foo"), new IntWritable(2))
+	                .runTest();
+	    }
+
+	 */
+}
